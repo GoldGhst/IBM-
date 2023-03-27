723bcb84c93ac6b9286ac4d7fae94dae0dbe4774
GoldGhst <115808623+GoldGhst@users.noreply.github.com>
2023-03-27 13:18:16 -0400

    Add files via upload

---
+++ IBM.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# IBM FINAL ASSIGNMENT
+# 
+
+# Zachary Wallace
+# 
+# Data Scientist
+# 
+# From my days at college I always enjoyed collecting data and using excel. I really enjoyed making visualizations and making data sets.
+
+# Ordered List
+# 1. numpy
+# 2. pandas
+# 3. PYtorch
+# 4. Tensorflow
+
+# |Data Science Tools|
+# | --- |
+# | Tableu     |
+# | TensorFlow |
+# | Jupyter    |
+# | Python     |
+# 
+
+# # Calculate the sum of 2 + 2
+
+# In[4]:
+
+
+2 + 2 
+
+
+# In[5]:
+
+
+2 * 2
+
+
+# In[7]:
+
+
+total_minutes = 120
+
+
+# In[8]:
+
+
+hours = total_minutes / 60
+
+
+# In[9]:
+
+
+print(hours)
+
+
+# # Unordered list
+
+# | A |
+# |---|
+# | 1 |
+# | 6 |
+# | 9 |
+# | 3 |
+
+# Zachary Wallace
+
+# In[ ]:
+
+
+
+
